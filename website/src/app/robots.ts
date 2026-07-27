c1530f0d812292d3366e47c29ae896c9420922e4
OneMoreJack <jiakeli1024@gmail.com>
2026-07-27 16:25:21 +0800

    refactor: export marketing site as static files

--- website/src/app/robots.ts
+++ website/src/app/robots.ts
@@ -2,12 +2,13 @@ import type { MetadataRoute } from "next";
 
 const siteUrl = "https://phototend.onemorejack.top";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/download/"],
     },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
--- website/src/app/sitemap.ts
+++ website/src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 const siteUrl = "https://phototend.onemorejack.top";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
